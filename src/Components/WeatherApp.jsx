8f65fb75b6b43bbf362616d13bb3e93b08d4c755
Manash Dutta <manash_dutta@msn.com>
2024-11-30 09:44:47 +0530

    handle bad requests

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -1,40 +1,139 @@
 import sunny from "../assets/images/sunny.png";
-import cliudy from "../assets/images/cloudy.png";
+import cloudy from "../assets/images/cloudy.png";
 import rainy from "../assets/images/rainy.png";
 import snowy from "../assets/images/snowy.png";
+import { useEffect, useState } from "react";
 
 const WeatherApp = () => {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  useEffect(() => {
+    const fetchDefaultWeather = async () => {
+      const defaultLocation = "London";
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${defaultLocation}&units=metric&appid=${apiKey}`;
+      const response = await fetch(url);
+      const defaultData = await response.json();
+      setData(defaultData);
+    };
+
+    fetchDefaultWeather();
+  }, []);
+
+  const handleInputChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const search = async () => {
+    if (location.trim() === "") return;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    setData(data);
+    setLocation("");
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      search();
+    }
+  };
+
+  const weatherImages = {
+    Clear: sunny,
+    Clouds: cloudy,
+    Rain: rainy,
+    Snow: snowy,
+    Haze: cloudy,
+    mist: cloudy,
+  };
+
+  const weatherImage = data.weather
+    ? weatherImages[data.weather[0].main]
+    : null;
+
+  const backgroundImages = {
+    Clear: "linear-gradient(to right, #f3b07c, #fcd283)",
+    Clouds: "linear-gradient(to right, #57d6d4, #71eeec)",
+    Rain: "linear-gradient(to right, #5bc8fb, #80eaff)",
+    Snow: "linear-gradient(to right, #aff2ff, #fff)",
+    Haze: "linear-gradient(to right, #57d6d4, #71eeec)",
+    mist: "linear-gradient(to right, #57d6d4, #71eeec)",
+  };
+
+  const backgroundImage = data.weather
+    ? backgroundImages[data.weather[0].main]
+    : backgroundImages[0];
+
+  const timestamp = data.dt;
+  const localDate = new Date(timestamp * 1000);
+  const options = {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  };
+  const date = localDate.toLocaleDateString("en-US", options);
+  const time = localDate.toLocaleTimeString()
+
+  console.log(data);
+
   return (
-    <div className="container">
-      <div className="weather-app">
+    <div className="container" style={{ backgroundImage }}>
+      <div
+        className="weather-app"
+        style={{
+          backgroundImage:
+            backgroundImage && backgroundImage.replace
+              ? backgroundImage.replace("to right", "to top")
+              : null,
+        }}
+      >
         <div className="search">
           <div className="search-top">
             <i className="fa-solid fa-location-dot"></i>
-            <div className="location">London</div>
+            <div className="location">{data.name}</div>
           </div>
           <div className="search-bar">
-            <input type="text" placeholder="Enter Location " />
-            <i className="fa-solid fa-magnifying-glass"></i>
+            <input
+              type="text"
+              placeholder="Enter Location "
+              value={location}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+            />
+            <i className="fa-solid fa-magnifying-glass" onClick={search}></i>
           </div>
         </div>
         <div className="weather">
-          <img src={sunny} alt="Sunny Weather" />
-          <div className="weather-type">Clear</div>
-          <div className="temp">28°</div>
+          <img src={weatherImage} alt="Sunny Weather" />
+          <div className="weather-type">
+            {data.weather ? data.weather[0].main : null}
+          </div>
+          <div className="temp">
+            {data.main ? `${Math.round(data.main.temp)}°` : null}
+          </div>
         </div>
         <div className="weather-date">
-          <p>Fri, Nov 29, 2024</p>
+          <p>{data.dt && date}</p>
+          <p>{data.dt && time}</p>
         </div>
         <div className="weather-data">
           <div className="humidity">
             <div className="data-name">Humidity</div>
             <i className="fa-solid fa-droplet"></i>
-            <div className="data">35%</div>
+            <div className="data">
+              {data.main ? `${data.main.humidity}%` : null}
+            </div>
           </div>
           <div className="wind">
             <div className="data-name">Wind</div>
             <i className="fa-solid fa-wind"></i>
-            <div className="data">3 km/h</div>
+            <div className="data">
+              {data.wind ? `${data.wind.speed} km/h` : null}
+            </div>
           </div>
         </div>
       </div>
